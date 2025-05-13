b5ec89dcaf324d9119016755c39fcbc192823e58
Teayeon-Lim <wingofyeon@naver.com>
2025-05-13 19:32:37 +0900

    chore: Add vitest devDependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-
+  test: {
+    globals: true,
+  },
   // Using github pages
   base: "/svg-interpolate-demo/",
 
